4541cbc47c18bf7120269efce9702e8dfcbd284d
Nils Druyen <n.druyen@nanogiants.de>
2021-12-02 10:34:35 +0100

    Day02 part2 solution

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,6 +1,40 @@
-val day02List = readInput("Day02").map {
-  val split = it.split(" ")
-  Step(split[0].toDirection(), split[1].toInt())
+fun main() {
+  val day02List = readInput("Day02").map {
+    val split = it.split(" ")
+    Step(split[0].toDirection(), split[1].toInt())
+  }
+
+  part1(day02List)
+  part2(day02List)
+}
+
+fun part1(inputList: List<Step>) {
+  val map = inputList.groupBy { it.direction }
+
+  val horizontal = map[Direction.FORWARD].sum()
+  val depth = map[Direction.DOWN].sum() - map[Direction.UP].sum()
+  val result = horizontal * depth
+
+  println("part1 result: $horizontal * $depth = $result")
+}
+
+fun part2(inputList: List<Step>) {
+  var aim = 0
+  var horizontal = 0
+  var depth = 0
+
+  inputList.forEach {
+    when (it.direction) {
+      Direction.DOWN -> aim += it.depth
+      Direction.UP -> aim -= it.depth
+      Direction.FORWARD -> {
+        horizontal += it.depth
+        depth += (aim * it.depth)
+      }
+    }
+  }
+
+  println("part2 result: $horizontal * $depth = ${horizontal * depth}")
 }
 
 data class Step(val direction: Direction, val depth: Int)
@@ -17,13 +51,3 @@ fun String.toDirection(): Direction = when (this) {
 }
 
 fun List<Step>?.sum(): Int = this?.sumOf { it.depth } ?: 0
-
-fun main() {
-  val map = day02List.groupBy { it.direction }
-
-  val horizontal = map[Direction.FORWARD].sum()
-  val depth = map[Direction.DOWN].sum() - map[Direction.UP].sum()
-  val result = horizontal * depth
-
-  println("horizontal/depth: $horizontal * $depth = $result")
-}
