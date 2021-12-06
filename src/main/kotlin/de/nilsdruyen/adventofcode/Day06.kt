9b738c3656eda602512943c322c588590e40bee1
Nils Druyen <n.druyen@nanogiants.de>
2021-12-06 14:55:40 +0100

    Cleanup Day06 code

--- src/main/kotlin/de/nilsdruyen/adventofcode/Day06.kt
+++ src/main/kotlin/de/nilsdruyen/adventofcode/Day06.kt
@@ -7,45 +7,40 @@ fun main() {
     line.split(",").map { it.toInt() }
   }.flatten().groupBy { it }
 
-  val groupFishCountByDayState = (0..8).toList().map {
+  val groupFishiesByDayState = (0..8).toList().map {
     // count fishies by internal timer
     inputList.filter { pair -> pair.key == it }.map { it.value }.flatten().count()
   }.map { it.toULong() }
 
-  Day06.part1(groupFishCountByDayState)
-  Day06.part2(groupFishCountByDayState)
+  Day06.part1(groupFishiesByDayState)
+  Day06.part2(groupFishiesByDayState)
 }
 
 object Day06 {
 
   fun part1(inputList: List<ULong>) {
     var resultList = inputList.toList()
-
     repeat(80) { resultList = resultList.processDay() }
-
     println("result: ${resultList.sumOf { it }}")
   }
 
   fun part2(inputList: List<ULong>) {
     var resultList = inputList.toList()
-
     repeat(256) { resultList = resultList.processDay() }
-
     println("result: ${resultList.sumOf { it }}")
   }
 }
 
 fun List<ULong>.processDay(): List<ULong> {
   val newFishies = this[0]
-
   return mutableListOf<ULong>().apply {
     repeat(8) {
-      if (it == 6) {
+      if (it == 6) { // resetted fishies + new fishies with day state 7
         add(this@processDay[it + 1] + newFishies)
       } else {
-        add(this@processDay[it + 1])
+        add(this@processDay[it + 1]) // move fishies on step closer to duplication
       }
     }
-    this.add(8, newFishies)
+    this.add(8, newFishies) // add new fishies
   }
 }
