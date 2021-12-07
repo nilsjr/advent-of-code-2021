f9e4105b85bc9f41134140f31ed31d60ee07fe65
Nils Druyen <n.druyen@nanogiants.de>
2021-12-07 10:05:40 +0100

    Day07 part 2 solution + refactoring

--- src/main/kotlin/de/nilsdruyen/adventofcode/Day07.kt
+++ src/main/kotlin/de/nilsdruyen/adventofcode/Day07.kt
@@ -2,7 +2,6 @@ package de.nilsdruyen.adventofcode
 
 import de.nilsdruyen.adventofcode.base.readInput
 import kotlin.math.abs
-import kotlin.math.roundToInt
 
 fun main() {
   val inputList = readInput("Day07").map { line ->
@@ -16,21 +15,29 @@ fun main() {
 object Day07 {
 
   fun part1(inputList: List<Int>) {
-    val median = medianOf(inputList)
-    val fuelConsumption = inputList.sumOf { abs(it - median) }
+    val fuelConsumption = inputList.minFuelConsumption { position, targetPosition ->
+      abs(position - targetPosition)
+    }
     println("result: $fuelConsumption")
   }
 
   fun part2(inputList: List<Int>) {
-    TODO()
+    val minFuelConsumption = inputList.minFuelConsumption { position, targetPosition ->
+      val diff = abs(position - targetPosition)
+      var fuelConsumption = 0
+      repeat(diff) { step -> fuelConsumption += step + 1 }
+      fuelConsumption
+    }
+    println("result: $minFuelConsumption")
   }
 }
 
-private fun medianOf(numbers: List<Int>): Int {
-  val list = numbers.sorted()
-  return if (list.size % 2 == 0) {
-    ((list[list.size / 2] + list[list.size / 2 - 1]) / 2).toDouble()
-  } else {
-    (list[list.size / 2]).toDouble()
-  }.roundToInt()
+fun List<Int>.minFuelConsumption(computation: (position: Int, targetPosition: Int) -> Int): Int {
+  val maxPosition = maxOrNull() ?: 0
+  val minPosition = minOrNull() ?: 1
+  val possiblePositions = (minPosition..maxPosition).toList()
+  return possiblePositions.minOfOrNull { targetPosition ->
+    // calculate fuel consumption for each possible position
+    sumOf { computation(it, targetPosition) }
+  } ?: 0
 }
