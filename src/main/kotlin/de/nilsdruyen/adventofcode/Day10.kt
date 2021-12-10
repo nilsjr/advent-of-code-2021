fda0e5c3e4f12943b3309cf6adf4361f0ec3ae48
Nils Druyen <n.druyen@nanogiants.de>
2021-12-10 08:49:17 +0100

    Add Day10 boilerplate

---
+++ src/main/kotlin/de/nilsdruyen/adventofcode/Day10.kt
@@ -0,0 +1,25 @@
+package de.nilsdruyen.adventofcode
+
+import de.nilsdruyen.adventofcode.base.readInput
+
+fun main() {
+  val inputList = readInput("Day10").map { line ->
+    // TODO: 04.12.2021 implement mapping
+    line
+  }
+
+  Day10.part1(inputList)
+  Day10.part2(inputList)
+}
+
+object Day10 {
+
+  fun part1(inputList: List<String>) {
+    TODO()
+  }
+
+  fun part2(inputList: List<String>) {
+    TODO()
+  }
+}
+
