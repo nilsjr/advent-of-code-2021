b76e66d4f34f228e8d7780ec0bd449a1c70b4e2e
Nils Druyen <n.druyen@nanogiants.de>
2021-12-13 22:34:37 +0100

    Day 12 part 2 solution

--- src/main/kotlin/de/nilsdruyen/adventofcode/Day12.kt
+++ src/main/kotlin/de/nilsdruyen/adventofcode/Day12.kt
@@ -9,47 +9,65 @@ fun main() {
   }
 
   Day12.part1(inputList)
-//  Day12.part2(inputList)
+  Day12.part2(inputList)
 }
 
 const val START = "start"
 const val END = "end"
 
-
 object Day12 {
 
   fun part1(inputList: List<Pair<String, String>>) {
-    val result = inputList.findPaths()
+    val result = inputList.findPaths(false)
     println(result.size)
   }
 
-  fun part2(inputList: List<String>) {
-    TODO()
+  fun part2(inputList: List<Pair<String, String>>) {
+    val result = inputList.findPaths(true)
+    println(result.size)
   }
 }
 
-fun List<Pair<String, String>>.findPaths(): List<String> {
+fun List<Pair<String, String>>.findPaths(part2: Boolean): List<String> {
   return map { listOf(it.first, it.second) }
     .flatten()
     .distinct()
     .associateWith { point ->
       mapNotNull { if (it.first == point) it.second else if (it.second == point) it.first else null }
     }
-    .combine()
+    .combine(part2)
     .filter { it.isNotEmpty() }
     .map { it.joinToString(",") }
     .distinct()
 }
 
-fun Map<String, List<String>>.combine(): List<List<String>> {
-  return getOrDefault(START, emptyList()).map { listOf(START, it).combine(this) }.flatten()
+fun Map<String, List<String>>.combine(part2: Boolean): List<List<String>> {
+  return getOrDefault(START, emptyList()).map { listOf(START, it).combine(this, part2) }.flatten()
 }
 
-fun List<String>.combine(inputMap: Map<String, List<String>>): List<List<String>> {
+fun List<String>.combine(inputMap: Map<String, List<String>>, part2: Boolean): List<List<String>> {
   val availablePoints = inputMap.getOrDefault(last(), emptyList())
-  val visitedSmallCaves = this.filter { it.toCharArray().all { char -> char.isLowerCase() } }
-  return availablePoints.filter { it !in visitedSmallCaves && it != START }.map {
+  val visitedSmallCaves = filterLowerCase()
+  val possibleCaves = if (part2) {
+    val visitedTwice = this.filterLowerCase().map { lowercaseString ->
+      lowercaseString to count { it == lowercaseString }
+    }.firstOrNull { it.second == 2 }?.first ?: ""
+
+    if (visitedTwice.isNotEmpty()) {
+      availablePoints.filter { it !in visitedSmallCaves && it != visitedTwice }
+    } else {
+      availablePoints
+    }
+  } else {
+    availablePoints.filter { it !in visitedSmallCaves }
+  }
+
+  return possibleCaves.filter { it != START }.map {
     val path = this + it
-    if (it == END) listOf(path) else path.combine(inputMap)
+    if (it == END) listOf(path) else path.combine(inputMap, part2)
   }.flatten()
 }
+
+fun String.isLowerCase(): Boolean = toCharArray().all(Char::isLowerCase)
+
+fun List<String>.filterLowerCase(): List<String> = filter(String::isLowerCase)
