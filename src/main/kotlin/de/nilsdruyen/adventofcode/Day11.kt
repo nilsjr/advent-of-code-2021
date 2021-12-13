2ba993619e27e4c2a4bdff49b4f0b22cd1829cbc
Nils Druyen <n.druyen@nanogiants.de>
2021-12-13 10:07:41 +0100

    Day11 solution not found yet -.-

--- src/main/kotlin/de/nilsdruyen/adventofcode/Day11.kt
+++ src/main/kotlin/de/nilsdruyen/adventofcode/Day11.kt
@@ -7,7 +7,7 @@ fun main() {
     line.map { it.toString().toInt() }.mapIndexed { colIndex, level ->
       Octupus(colIndex, rowIndex, level)
     }
-  }
+  }.flatten()
 
   Day11.part1(inputList)
 //  Day11.part2(inputList)
@@ -15,25 +15,34 @@ fun main() {
 
 object Day11 {
 
-  fun part1(inputList: List<List<Octupus>>) {
+  fun part1(inputList: List<Octupus>) {
+    var flashes = 0
     repeat(2) {
-      inputList.forEach { row -> row.forEach { it.increaseEnergyLevel() } }
+      inputList.forEach { it.energyLevel++ }
 
-      while (inputList.any { row -> row.any { it.canFlash() } }) {
-//        println(inputList.count { it.canFlash() })
-//        println("flash")
-        inputList.print()
-        println()
-        inputList.forEach { row -> row.forEach { it.flash(inputList) } }
+      while (inputList.any { it.energyLevel >= 10 }) {
+        val flashies = inputList.filter { it.energyLevel >= 10 }
+        flashes += flashies.size
+
+        flashies.forEach { octupus ->
+          octupus.energyLevel = 0
+          octupus
+            .neighbours(inputList)
+            .mapNotNull { inputList.getOrNull(it) }
+            .filter { it.energyLevel != 0 }
+            .forEach { it.energyLevel++ }
+        }
       }
 
-      inputList.forEach { row -> row.forEach { it.reset() } }
+      inputList.print()
+      println()
     }
 
-//    val matrix = inputList.windowed(10, 10)
-//    println(matrix[0].joinToString("") { it.energyLevel.toString() } == "8807476555")
-//    println(matrix[1].joinToString("") { it.energyLevel.toString() } == "5089087054")
-//    println(inputList.sumOf { it.flashCount })
+    val matrix = inputList.windowed(10, 10)
+    println(matrix[0].joinToString("") { it.energyLevel.toString() } == "8807476555")
+    println(matrix[1].joinToString("") { it.energyLevel.toString() } == "5089087054")
+
+    println(flashes)
   }
 
   fun part2(inputList: List<Octupus>) {
@@ -45,83 +54,38 @@ data class Octupus(
   val x: Int,
   val y: Int,
   var energyLevel: Int,
-  var flashed: Boolean = false,
-  var flashCount: Int = 0
-) {
+)
 
-  fun increaseEnergyLevel() {
-    if (energyLevel <= 9) energyLevel++
-  }
-
-  fun canFlash(): Boolean = energyLevel > 9 && !flashed
-
-//  fun flash(inputList: List<Octupus>) {
-//    if (energyLevel > 9 && !flashed) {
-//      flashed = true
-//      flashCount++
-//      this.neighboursFrom(inputList).forEach { it.increaseEnergyLevel() }
-//    }
-//  }
-
-  fun flash(inputList: List<List<Octupus>>) {
-    if (energyLevel > 9 && !flashed) {
-      flashed = true
-      flashCount++
-      this.neighboursFrom(inputList).forEach { it.increaseEnergyLevel() }
-    }
-  }
-
-  fun reset() {
-    if (energyLevel > 9) {
-      energyLevel = 0
-      flashed = false
-    }
-  }
+fun List<List<Octupus>>.forEachOctupus(block: (Octupus) -> Unit) {
+  forEach { row -> row.forEach { block(it) } }
 }
 
-//fun List<Octupus>.print() {
-//  val width = maxOf { it.y } + 1
-//  windowed(width, width).forEach { list ->
-//    println(list.joinToString("") {
-//      if (it.energyLevel >= 10) "X" else it.energyLevel.toString()
-//    })
-//  }
-//}
+fun List<List<Octupus>>.filterOctupus(block: (Octupus) -> Boolean) {
+  filter { row -> row.all { block(it) } }
+}
 
-fun List<List<Octupus>>.print() {
-  forEach { row ->
-    println(row.joinToString("") {
+fun List<Octupus>.print() {
+  val width = maxOf { it.y } + 1
+  windowed(width, width).take(3).forEach { list ->
+    println(list.joinToString("") {
       if (it.energyLevel >= 10) "X" else it.energyLevel.toString()
     })
   }
 }
 
-//fun Octupus.neighboursFrom(list: List<Octupus>): List<Octupus> {
-//  val width = list.maxOf { it.y } + 1
-//  return listOfNotNull(
-//    // top neighbours
-//    list.getOrNull((y - 1) + (x - 1) * width),
-//    list.getOrNull(y + (x - 1) * width),
-//    list.getOrNull((y + 1) + (x - 1) * width),
-//    // bottom neighbours
-//    list.getOrNull((y - 1) + (x + 1) * width),
-//    list.getOrNull(y + (x + 1) * width),
-//    list.getOrNull((y + 1) + (x + 1) * width),
-//    // left right
-//    list.getOrNull((y - 1) + x * width),
-//    list.getOrNull((y + 1) + x * width),
-//  ).filter { !it.canFlash() }
-//}
-
-fun Octupus.neighboursFrom(list: List<List<Octupus>>): List<Octupus> {
-  return listOfNotNull(
-    list.getOrNull(x - 1)?.getOrNull(y - 1),
-    list.getOrNull(x - 1)?.getOrNull(y),
-    list.getOrNull(x - 1)?.getOrNull(y + 1),
-    list.getOrNull(x + 1)?.getOrNull(y - 1),
-    list.getOrNull(x + 1)?.getOrNull(y),
-    list.getOrNull(x + 1)?.getOrNull(y + 1),
-    list.getOrNull(x)?.getOrNull(y - 1),
-    list.getOrNull(x)?.getOrNull(y + 1),
-  ).filter { it.energyLevel <= 9 }.also { println("$x $y $energyLevel neighbours: ${it.size}") }
+fun Octupus.neighbours(list: List<Octupus>): List<Int> {
+  val width = list.maxOf { it.y } + 1
+  return listOf(
+    // top neighbours
+    (y - 1) + (x - 1) * width,
+    y + (x - 1) * width,
+    (y + 1) + (x - 1) * width,
+    // bottom neighbours
+    (y - 1) + (x + 1) * width,
+    y + (x + 1) * width,
+    (y + 1) + (x + 1) * width,
+    // left right
+    (y - 1) + x * width,
+    (y + 1) + x * width,
+  )
 }
