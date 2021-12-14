51444c553075d621a72b7be4b5886312d7a66c5a
Nils Druyen <n.druyen@nanogiants.de>
2021-12-14 09:14:13 +0100

    Add Day14 boilerplate

---
+++ src/main/kotlin/de/nilsdruyen/adventofcode/Day14.kt
@@ -0,0 +1,25 @@
+package de.nilsdruyen.adventofcode
+
+import de.nilsdruyen.adventofcode.base.readInput
+
+fun main() {
+  val inputList = readInput("Day14test").map { line ->
+    // TODO: 04.12.2021 implement mapping
+    line
+  }
+
+  Day14.part1(inputList)
+  Day14.part2(inputList)
+}
+
+object Day14 {
+
+  fun part1(inputList: List<String>) {
+    TODO()
+  }
+
+  fun part2(inputList: List<String>) {
+    TODO()
+  }
+}
+
