0a5643e18e04580b3d931085f6521b847be7c398
Nils Druyen <info@nilsdruyen.de>
2021-12-14 22:44:03 +0100

    Day14 part 2 solution

--- src/main/kotlin/de/nilsdruyen/adventofcode/Day14.kt
+++ src/main/kotlin/de/nilsdruyen/adventofcode/Day14.kt
@@ -1,60 +1,47 @@
 package de.nilsdruyen.adventofcode
 
 import de.nilsdruyen.adventofcode.base.readInput
+import kotlin.math.roundToLong
 
 fun main() {
-  val inputList = readInput("Day14test")
-  val template = inputList.first().windowed(2, 1)
-  val insertions = inputList.drop(2).map {
-    val (from, to) = it.split(" -> ")
-    from to to
-  }.toMap()
+  val inputList = readInput("Day14")
 
-  Day14.part1(template, insertions)
-  Day14.part2(template, insertions)
+  val startTemplate = inputList.first().windowed(2)
+  val ruleList = inputList.drop(2).associate {
+    val (from, to) = it.split(" -> ")
+    from to listOf("${from[0]}$to", "$to${from[1]}")
+  }
+
+  Day14.part1(startTemplate, ruleList)
+  Day14.part2(startTemplate, ruleList)
 }
 
 object Day14 {
 
-  fun part1(inputList: List<String>, insertions: Map<String, String>) {
-    val charCount = inputList.buildAndCount(insertions, 10)
-    println(charCount.maxOf { it.value } - charCount.minOf { it.value })
+  fun part1(startTemplate: List<String>, ruleList: Map<String, List<String>>) {
+    println(buildAndCount(startTemplate, ruleList, 10))
   }
 
-  fun part2(inputList: List<String>, insertions: Map<String, String>) {
-    val charCount = inputList.buildAndCount(insertions, 20)
-    println(charCount.maxOf { it.value } - charCount.minOf { it.value })
+  fun part2(startTemplate: List<String>, ruleList: Map<String, List<String>>) {
+    println(buildAndCount(startTemplate, ruleList, 40))
   }
 }
 
-fun List<String>.buildAndCount(insertions: Map<String, String>, times: Int): Map<String, Int> {
-  val charCount = mutableMapOf<String, Int>()
+fun buildAndCount(startTemplate: List<String>, ruleList: Map<String, List<String>>, times: Int): Long {
+  var polymer = startTemplate.groupBy { it }.mapValues { it.value.size.toLong() }
 
-  forEach { part ->
-    println(part)
-    var chunk = part.map { it.toString() }
-    repeat(times) {
-      chunk = buildList {
-        chunk.windowed(2, 1).forEach { (first, second) ->
-          val inBetween = insertions.getOrDefault("$first$second", "")
-          if (isNotEmpty() && last() == first) {
-            add(inBetween)
-            add(second)
-          } else {
-            add(first)
-            add(inBetween)
-            add(second)
-          }
-        }
-      }
-      println(it)
-    }
-    chunk.countChars().forEach { (t, u) ->
-      charCount[t] = u + (charCount[t] ?: 0)
-    }
+  repeat(times) {
+    polymer = polymer
+      .flatMap { (str, count) -> ruleList.getValue(str).map { it to count } }
+      .groupBy { it.first }
+      .mapValues { it.value.sumOf { it.second } }
   }
-  return charCount
+
+  val countList = polymer
+    .flatMap { (chunk, count) -> listOf(chunk[0] to count, chunk[1] to count) }
+    .groupBy { it.first }
+    .map { char -> (char.value.sumOf { it.second } / 2.0).roundToLong() }
+    .sorted()
+
+  return countList.last() - countList.first()
 }
-
-fun List<String>.countChars(): Map<String, Int> = joinToString("").groupingBy { it.toString() }.eachCount()
-
